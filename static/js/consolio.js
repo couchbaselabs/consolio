4eb56b26ef00edb96eb73080afb0b9d1902eac49
Dustin Sallings <dustin@spy.net>
2013-06-12 17:12:42 -0700

    Added bucket creation form

--- static/js/consolio.js
+++ static/js/consolio.js
@@ -17,6 +17,29 @@ angular.module("consolio", ['consAuth']).
 function ConsolioCtrl($scope, $http, $rootScope, consAuth) {
     $rootScope.$watch('loggedin', function() {
         $scope.auth = consAuth.get(); });
+
+    $http.get("/api/my/databases/").success(function(databases) {
+        $scope.databases = databases;
+    });
+
+    $scope.newbucket = "";
+
+    $scope.newdb = function() {
+        var dbname = $("#newbucketname");
+        console.log("Adding a new thing");
+        $http.post('/api/database/new/',
+                   'name=' + encodeURIComponent(dbname),
+                   {headers: {"Content-Type": "application/x-www-form-urlencoded"}})
+            .error(function(data, code) {
+                bAlert("Error " + code, "Couldn't create " + dbname +
+                       ": " + data, "error");
+            })
+            .success(function(data) {
+                console.log("Worked!");
+                $scope.newbucket="";
+                $scope.databases.push(data);
+            });
+    };
 }
 
 
