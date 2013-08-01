7f7d0d002bda998cb3768bc2043fcb6eb82238b9
Dustin Sallings <dustin@spy.net>
2013-07-31 19:46:25 -0700

    Send the generated bucket name as bucketname

--- sync_gateway.go
+++ sync_gateway.go
@@ -83,6 +83,7 @@ func handleNewSGW(w http.ResponseWriter, req *http.Request) {
 		d.ExtraInfo["db_pass"] = bucket.Password
 		d.ExtraInfo["server"] = bucket.URL
 		d.ExtraInfo["generated_db"] = bucket.Name
+		d.ExtraInfo["dbname"] = bucket.Name
 	}
 
 	if b, _ := strconv.ParseBool(req.FormValue("guest")); b {
