f793c4f781ba9f4fdf49cd50eba9e08f3eb964ee
Dustin Sallings <dustin@spy.net>
2013-08-29 00:56:18 -0700

    Build dynamic configuration for cbgb

--- tools/confproxy/confproxy.go
+++ tools/confproxy/confproxy.go
@@ -15,15 +15,19 @@ var (
 	keyRingPath = flag.String("keyring", "", "Your secret keyring")
 	keyPassword = flag.String("password", "", "Crypto password")
 	bindAddr    = flag.String("bind", ":8475", "Binding address")
+	confType    = flag.String("type", "sgw", "Configuration type (sgw|cbgb)")
 )
 
-type Database struct {
-	Sync, Users    *json.RawMessage
-	DBName, Server string
-	Pass           string `json:"db_pass"`
+type SyncGateway struct {
+	Extra struct {
+		Sync, Users    *json.RawMessage
+		DBName, Server string
+		Pass           string `json:"db_pass"`
+	}
 }
 
-func (d Database) MarshalJSON() ([]byte, error) {
+func (s *SyncGateway) MarshalJSON() ([]byte, error) {
+	d := s.Extra
 	m := map[string]interface{}{}
 	m["bucket"] = d.DBName
 	m["sync"] = d.Sync
@@ -47,7 +51,36 @@ func (d Database) MarshalJSON() ([]byte, error) {
 	return json.Marshal(m)
 }
 
-func getit(u string) (*Database, error) {
+type Database struct {
+	Password string
+}
+
+func (d *Database) MarshalJSON() ([]byte, error) {
+	m := map[string]interface{}{
+		"memoryOnly":       0,
+		"numPartitions":    1,
+		"passwordHash":     "",
+		"passwordHashFunc": "",
+		"passwordSalt":     "",
+		"quotaBytes":       500 * 1024 * 1024,
+	}
+
+	pass, err := consoliotools.Decrypt(d.Password)
+	if err == nil {
+		m["passwordHash"] = pass
+	} else {
+		log.Printf("Error decrypting password: %v", err)
+	}
+
+	return json.Marshal(m)
+}
+
+var obGens = map[string]func() interface{}{
+	"sgw":  func() interface{} { return &SyncGateway{} },
+	"cbgb": func() interface{} { return &Database{} },
+}
+
+func getit(u string) (interface{}, error) {
 	res, err := http.Get(u)
 	if err != nil {
 		return nil, err
@@ -60,16 +93,17 @@ func getit(u string) (*Database, error) {
 	d := json.NewDecoder(res.Body)
 	d.UseNumber()
 
-	reson := struct {
-		Name  string
-		Extra Database
-	}{}
-	err = d.Decode(&reson)
-	return &reson.Extra, nil
+	db := obGens[*confType]()
+	err = d.Decode(db)
+	return db, nil
 }
 
 func main() {
 	flag.Parse()
+
+	if _, ok := obGens[*confType]; !ok {
+		log.Fatalf("Invalid conf type: %v", *confType)
+	}
 
 	consoliotools.InitCrypto(*keyRingPath, *keyPassword)
 	baseUrl := flag.Arg(0)
