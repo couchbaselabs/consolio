80008147ebc6a7defec754802e6af7721c8151ce
Dustin Sallings <dustin@spy.net>
2013-09-12 14:40:18 -0700

    bysize administrative view

--- database.go
+++ database.go
@@ -8,7 +8,7 @@ import (
 
 const ddocKey = "consolio"
 const markerKey = "/@consolioddocVersion"
-const ddocVersion = 5
+const ddocVersion = 6
 const ddocBody = `{
   "id": "_design/consolio",
   "views": {
@@ -18,6 +18,9 @@ const ddocBody = `{
     "items": {
       "map": "function (doc, meta) {\n  if (doc.owner && doc.type && doc.name) {\n    emit([doc.type, doc.owner, doc.name], doc.size || 0);\n  }\n}",
       "reduce": "_sum"
+    },
+    "bysize": {
+      "map": "function (doc, meta) {\n  if (doc.stats && doc.stats.fileSize) {\n    var name = doc.extra.generated_for || doc.name;\n    emit(doc.stats.fileSize, name);\n  }\n}"
     }
   }
 }`
