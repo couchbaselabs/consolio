c22847e15a97e90fec20784c6692bf445af91dbd
Dustin Sallings <dustin@spy.net>
2013-09-16 16:02:49 -0700

    Fix broken view defs

--- database.go
+++ database.go
@@ -8,16 +8,15 @@ import (
 
 const ddocKey = "consolio"
 const markerKey = "/@consolioddocVersion"
-const ddocVersion = 8
+const ddocVersion = 9
 const ddocBody = `{
   "id": "_design/consolio",
   "views": {
     "items": {
-      "map": "function (doc, meta) {\n  if (doc.owner && doc.type && doc.name) {\n    emit([doc.type, doc.owner, doc.name], doc.size || 0);\n  }\n}",
-      "reduce": "_sum"
+      "map": "function (doc, meta) {\n  if (doc.owner && doc.type && doc.name) {\n    emit([doc.type, doc.owner, doc.name], null);\n  }\n}"
     },
     "bysize": {
-      "map": "function (doc, meta) {\n  if (doc.stats && doc.stats.fileSize) {\n    var name = doc.extra.generated_for || doc.name;\n    emit(doc.stats.fileSize, {name: name, owner: doc.owner});\n  }\n}"
+      "map": "function (doc, meta) {\n  if (doc.stats && doc.stats.fileSize) {\n    var name = (doc.extra && doc.extra.generated_for) || doc.name;\n    emit(doc.stats.fileSize, {name: name, owner: doc.owner});\n  }\n}"
     }
   }
 }`
